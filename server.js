b3b866120dbc47341ab461e44bbbf1bfa50d6802
Maryam Hanafiah <maryam@chilon.net>
2020-06-08 13:03:58 +0800

    added falling snow

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fastifyStatic = require('fastify-static')
 const path = require('path')
 
 fastify.register(fastifyStatic, {
-  root: path.join(__dirname, 'public')
+  root: path.join(__dirname, '')
 })
 
 const start = async () => {
