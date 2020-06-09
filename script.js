b323d346c8f6a3e9b150c3c9cf48559e9a546585
Meowyam <maryam@chilon.net>
2020-06-09 19:40:23 +0800

    working snowglobe, with animated house, moon and stars

--- script.js
+++ script.js
@@ -1,36 +1,142 @@
 let snowDepth = 0
+let shakeComplete = false
 
 const generateSnow = function() {
   let i = 0
-  let snowArr = []
   while (i < 15) {
-    document.getElementById('snowglobe').innerHTML += '<i class="fas fa-snowflake"></i>'
+    document.getElementById('snowbox').innerHTML += '<i class="fas fa-snowflake"></i>'
     i++
   }
 }
+generateSnow()
 
-function shakeGlobe(strength) {
-  anime({
-    targets: '#snowglobe',
-    // animation keyframes
+//swing the moon
+// pendulum animation is easy
+const moon = anime({
+  // target anything. here: a css selector
+  targets: '.moon',
+  //animate css properties
+  rotate: [30, -30],
+  duration: 500,
+  // Robert Penner's popular easing functions are built in
+  // robertpenner.com/easing
+  easing: 'easeInOutSine',
+  direction: 'alternate',
+  loop: true,
+})
+
+//animate svg
+//use the path as a motion path,
+//morph the path (shapes must have same number of points)
+//and line drawing (path must have stroke, cf svg line animation in css
+const house = anime({
+  targets: '.house svg path',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+})
+
+
+const twinkle = anime({
+  //property keyframes defined in property Object Array
+  //property keyframes can overlap
+  //here both size and colour change
+  targets: '.fa-star',
+  scale: [
+    {value: 2},
+    {value: 1},
+    {value: 3},
+    {value: 2},
+  ],
+  color: [
+    {value: '#ffffff'},
+    {value: '#b0ceff'},
+    {value: '#ffffff'},
+    {value: '#f2ff9c'},
+  ],
+  duration: 1000,
+  // loop: can enter a number or true for infinite
+  loop: true,
+  // normal, reverse, or alternate
+  direction: 'alternate',
+  easing: 'easeInOutSine',
+})
+
+
+//add shaking
+//once the instance is created, you can't change duration.
+//you have to create a new animation from scratch.
+
+//you can synch animations by adding animations on a timeline,
+//by default they chain, each starts after the previous animation ends
+//but you can add an offset
+//but you can't dynamically add animations to occur simultaneously
+
+//first define basic parameters: eg. duration, looping
+
+const fallingSnow = anime.timeline({
+  targets: '.fa-snowflake',
+  autoplay: false,
+  loop: true,
+  duration: 800,
+  easing: 'easeOutElastic(10,2)',
+  // a callback is triggered on every frame
+  // this callback is triggered when each loop ends
+  loopComplete: function() {
+    console.log(snowDepth)
+    snowDepth++
+    document.querySelector('#snowdepth').style.height = (snowDepth * 10) + 'px'
+    if (snowDepth == 40) {
+      anime.remove('.fa-snowflake')
+    }
+    return snowDepth
+  },
+})
+//add other animations as children
+// some parameters can be inherited: targets, easing, duration, delay, round (rounds up value)
+// you can override the inherited parameters when you declare new ones in the children
+
+//shake the globe
+  .add({
+    targets: '.snowglobe',
+  // animation keyframes
+  // keyframes are defined in an Array
     keyframes: [
-      {translateX: strength/5},
-      {translateX: -strength/5},
-      {translateX: strength/5},
-      {translateX: -strength/5},
+      {translateX: 100},
+      {translateX: -100},
+      {translateX: 80},
+      {translateX: -80},
+      {translateX: 60},
+      {translateX: -60},
+      {translateX: 40},
+      {translateX: 40},
       {translateX: 0}
     ],
-    duration: 500,
-    // easing spring(mass, stiffness, damping, velocity)
-    // the higher the mass, the 'stretchier'
+    duration: 200,
+  // easing spring(mass, stiffness, damping, velocity)
+  // the higher the mass, the 'stretchier'
     easing: 'spring(2, 2, 2, 10)',
+    // callback triggered when animation is completed
+    // looped timeline plays all children
+    complete: function(anim) {
+      if (anim.completed == true) {
+        anime.remove('.snowglobe')
+      }
+    }
   })
-}
 
-function fallingSnow(strength,loopable) {
-  anime({
-    // target anything. here: a css selector
-    targets: '.fa-snowflake',
+  // make snow visible
+  .add({
+    targets: '#snowbox',
+    opacity: 1,
+    duration: 1,
+    loop: true,
+  })
+
+  // when first animation ends
+  // continue to next in timeline
+
+  .add({
     // css values. here: opacity
     opacity: {
       value: 0,
@@ -48,106 +154,28 @@ function fallingSnow(strength,loopable) {
       // stagger multiple elements with follow through and overlapping action
       delay: anime.stagger(50),
       easing: 'linear',
+      loop: true,
     },
     rotateZ: {
-    // specific property parameters
+      // specific property parameters
       value: 360,
       duration: 3000,
       direction: 'alternate',
       easing: 'linear',
       loop: true,
     },
-    // keyframes are defined in an Array
-    // keyFrames for property translateX
-    translateX: [
-      {
-        value: strength,
-        duration: 100,
-        easing: 'linear',
-      },
-      {
-        value: -strength,
-        duration: 100,
-        easing: 'linear',
-      },
-      {
-        value: strength,
-        duration: 150,
-        easing: 'linear',
-      },
-      {
-        value: -strength,
-        duration: 150,
-        easing: 'linear',
-      },
-      //'If there is no duration specified inside the keyframes, each keyframe duration will be equal to the animation's total duration divided by the number of keyframes.'
-      {
-        value: 0,
-        easing: 'linear',
-      },
-    ],
-    // a callback is triggered on every frame
-    // begin callback is triggered when animation is started
-    loopBegin: function(anim) {
-      snowDepth++
-      showSnowdepth(snowDepth)
-    },
-    // complete callback
-    loopComplete: function(anim) {
-      totalSnow(snowDepth)
-    },
-    loop: loopable
   })
-}
 
-function showSnowdepth(snowDepth) {
-  anime({
-    targets: '.snowdepth',
-    height: snowDepth * 10,
-    // delay in milliseconds
-    delay: 2000,
-  })
-}
-
-
-//animate any Object with a numerical value
-
-let snowfallObj = {
-  snowCount: 0,
-}
-
-function totalSnow(snowDepth) {
-  console.log(snowDepth)
-  anime({
-    targets: snowfallObj,
+  .add({
+    //animate any Object with a numerical value
     snowCount: snowDepth,
     easing: 'easeOutElastic(10,2)',
     round: 1,
     update: function() {
-      document.querySelector('.totalSnowfall').innerHTML = snowfallObj.snowCount
+      document.querySelector('.totalSnowfall').innerHTML = snowDepth
     }
   })
-}
 
-generateSnow()
+document.querySelector('.play').onclick = fallingSnow.play
+document.querySelector('.pause').onclick = fallingSnow.pause
 
-function startSnow() {
-  let strength = 0
-  fallingSnow(strength,true)
-}
-
-//add shaking
-//once the instance is created, you can't change the duration.
-//you have to create a new animation from scratch.
-
-//you can CHAIN animations by adding animations on a timeline,
-//but not dynamically add animations to occur simultaneously
-
-function shake() {
-  console.log('shake')
-  strength = 50
-  fallingSnow(strength,false)
-  shakeGlobe(strength)
-}
-
-document.addEventListener("DOMContentLoaded", startSnow())
